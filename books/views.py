9a4e42ea985087e3d356aec3896918b7e2c6e028
Alex Kleider <alex@kleider.ca>
2017-09-24 18:39:22 -0700

    Got to end of chapter 3, p34.

--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_page(request):
+    return HttpResponse(
+    '<html><title>Double Entry Book Keeping</title></html>')
--- debk/urls.py
+++ debk/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from register.views import home_page
+from books.views import home_page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
