65fa66c5d071fa1857036e6a1e5263395c534421
Alex Kleider <alex@kleider.ca>
2017-10-03 09:08:58 -0700

    Add empty functions preparing for entity creation.

--- books/views.py
+++ books/views.py
@@ -3,8 +3,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def entity_created(entity_name):
+    return True
+
+def announce_failure(bad_entry):
+    """Inform user that entry wasn't accepted"""
+    pass
+
 def home_page(request):
+    new_entity = request.POST.get("new_entity", "")
+    if new_entity:
+        if not entity_created(new_entity):
+            announce_failure(new_entity)
+            new_entity = ''
     return render(request, "home.html", {
-        "new_entity_text": request.POST.get("new_entity", ""),
+        "new_entity_text": new_entity,
         })
 
